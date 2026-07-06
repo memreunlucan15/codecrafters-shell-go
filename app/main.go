5d63c2df822bfd67ea68c5c6fc885049fe41f1da
memreunlucan15 <mehmetemreunlucan@gmail.com>
2026-07-06 20:25:01 +0300

    [any message]

--- app/main.go
+++ app/main.go
@@ -33,6 +33,8 @@ func main() {
 				fmt.Println(strings.TrimPrefix(command, "type ") + " is a shell builtin")
 			} else if strings.HasSuffix(command, "type") {
 				fmt.Println(strings.TrimPrefix(command, "type ") + " is a shell builtin")
+			} else {
+				fmt.Println(strings.TrimPrefix(command + ": command not found"))
 			}
 
 		} else if command == "exit" {
