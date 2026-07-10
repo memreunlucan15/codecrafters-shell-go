ec4bb3d50fb1036b777d6cce93384abba7e2952a
memreunlucan15 <mehmetemreunlucan@gmail.com>
2026-07-10 21:56:23 +0300

    [any message]

--- app/main.go
+++ app/main.go
@@ -210,6 +210,8 @@ func (b *benimCompleter) Do(line []rune, pos int) ([][]rune, int) {
 	} else if len(oneriler) > 1 && b.tabSayisi == 1 {
 		fmt.Print("\x07")
 		oneriler = nil
+	} else if b.tabSayisi == 2 {
+		fmt.Print("$ " + b.oncekiPrefix)
 	}
 
 	b.oncekiPrefix = prefix
