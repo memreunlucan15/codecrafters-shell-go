5dda9247a85a44e9f25fa4384651a5571c73b5d2
memreunlucan15 <mehmetemreunlucan@gmail.com>
2026-07-11 19:47:17 +0300

    [test]

--- app/main.go
+++ app/main.go
@@ -234,8 +234,7 @@ func (b *benimCompleter) Do(line []rune, pos int) ([][]rune, int) {
 	} else if len(oneriler) > 1 && b.tabSayisi == 1 {
 		fmt.Print("\x07")
 		if strings.HasSuffix(prefix, "_") {
-			yazdir := oneriler[0]
-			fmt.Print(yazdir)
+			fmt.Print(strings.TrimSpace(string(oneriler[0])))
 			oneriler = nil
 		} else {
 
