7d355179d5f38af01e8023c0ac69974390e0cd5e
memreunlucan15 <mehmetemreunlucan@gmail.com>
2026-07-11 18:06:04 +0300

    [implementing partial completions]

--- app/main.go
+++ app/main.go
@@ -232,12 +232,13 @@ func (b *benimCompleter) Do(line []rune, pos int) ([][]rune, int) {
 
 		return oneriler, len(prefix)
 	} else if len(oneriler) > 1 && b.tabSayisi == 1 {
+		fmt.Print("\x07")
 		if strings.HasSuffix(prefix, "_") {
 
 			fmt.Print(oneriler[0])
 
 		} else {
-			fmt.Print("\x07")
+
 			oneriler = nil
 		}
 	} else if len(oneriler) > 1 && b.tabSayisi == 2 {
