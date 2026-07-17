88f5125f6fd3c4965c013a83bda6fd9102817910
memreunlucan15 <mehmetemreunlucan@gmail.com>
2026-07-17 19:45:35 +0300

    multiple completer candidates - made completer_script compatible with the rest of the code

--- app/main.go
+++ app/main.go
@@ -293,7 +293,12 @@ func (b *benimCompleter) Do(line []rune, pos int) ([][]rune, int) {
 			prog.Env = append(prog.Environ(), "COMP_LINE="+fullprefix)
 			prog.Env = append(prog.Environ(), "COMP_POINT="+strconv.Itoa(len(fullprefix)))
 			prog_output, _ = prog.Output()
-			if len(prog_output) > 1 {
+
+			conv := make([]string, len(prog_output))
+			for i, b := range prog_output {
+				conv[i] = string(b)
+			}
+			if len(conv) > 1 {
 				completer_script_mc = true
 			} else {
 
