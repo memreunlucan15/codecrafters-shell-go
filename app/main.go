af8f526a9f04307f5d840011db95093a063d3655
memreunlucan15 <mehmetemreunlucan@gmail.com>
2026-07-21 15:57:39 +0300

    read history from file

--- app/main.go
+++ app/main.go
@@ -504,7 +504,7 @@ func runBuiltin(tokens []string, out, outErr io.Writer) (ran bool, quit bool) {
 
 			if len(tokens) > 2 && tokens[1] == "-r" {
 				file, _ := os.ReadFile(tokens[2])
-				fTos := strings.Fields(string(file))
+				fTos := strings.Split(string(file), "\n")
 				for i := 0; i < len(fTos); i++ {
 					history_mem = append(history_mem, fTos[i])
 				}
